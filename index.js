3ed7d14ccf327fd19328e207e1b8d8c05ded6b99
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 21:16:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArr = [];
+  for ( var i = 0; i < musicians.length; i++ ) {
+    emptyArry.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return arr;
+}
